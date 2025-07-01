17be37c19414941a0edeb5a756f6c20e4c1c4dc2
verystark <tuomaspaloheimo05@gmail.com>
2025-07-01 17:47:12 +0300

    Train kNN model and calculate accuracy of model.

--- digits-ocr.py
+++ digits-ocr.py
@@ -12,3 +12,17 @@ x = np.array(cells)
 
 train = x[:,:50].reshape(-1, 400).astype(np.float32)
 test = x[:,50:100].reshape(-1,400).astype(np.float32)
+
+# Make the labels for each digit 0-9
+k = np.arange(10)
+train_label = np.repeat(k, 250)[:,np.newaxis]
+test_label = train_label.copy()
+
+knn = cv.ml.KNearest.create()
+knn.train(train, cv.ml.ROW_SAMPLE, train_label)
+ret, results, neighbours, dist = knn.findNearest(test, k = 5)
+
+matches = results == test_label
+correct = np.count_nonzero(matches)
+accuracy = correct / results.size * 100.0
+print(accuracy)
