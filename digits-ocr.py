efac486e71d9f3d717b57aef1433c4a30ac4f334
verystark <tuomaspaloheimo05@gmail.com>
2025-07-03 21:25:45 +0300

    Update handwritten digit recognition training images to 4000 and testing images to 1000

--- digits-ocr.py
+++ digits-ocr.py
@@ -1,6 +1,10 @@
 import numpy as np
 import cv2 as cv
+import tkinter as tk
+from sklearn.utils import shuffle
 
+root = tk.Tk()
+myCanvas = tk.Canvas(root, bg='white', height=400, width=400, bd=0, cursor='circle')
 img = cv.imread('digits.png')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -8,15 +12,15 @@ cells = []
 for row in np.vsplit(gray, 50):
     cells.append(np.hsplit(row, 100))
 
-x = np.array(cells)
+x = np.array(cells).reshape(-1, 400).astype(np.float32)
 
-train = x[:,:50].reshape(-1, 400).astype(np.float32)
-test = x[:,50:100].reshape(-1,400).astype(np.float32)
+labels = np.repeat(np.arange(10), 500)[:, np.newaxis]
+x, labels = shuffle(x, labels, random_state=42)
 
-# Make the labels for each digit 0-9
-k = np.arange(10)
-train_label = np.repeat(k, 250)[:,np.newaxis]
-test_label = train_label.copy()
+train = x[:4000]
+test = x[4000:]
+train_label = labels[:4000]
+test_label = labels[4000:]
 
 knn = cv.ml.KNearest.create()
 knn.train(train, cv.ml.ROW_SAMPLE, train_label)
@@ -26,3 +30,4 @@ matches = results == test_label
 correct = np.count_nonzero(matches)
 accuracy = correct / results.size * 100.0
 print(accuracy)
+
